6cc566e5654fce00c023dfa85c6783c7f91e62c1
abhirock81 <abhishekrock81@gmail.com>
2018-08-05 22:05:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a1605417/chatbot/MainActivity.java
@@ -0,0 +1,357 @@
+package com.example.a1605417.chatbot;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
+
+import ai.api.AIDataService;
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+
+public class MainActivity extends AppCompatActivity implements AIListener,TextToSpeech.OnInitListener{
+
+    RecyclerView recyclerView;
+    EditText editText;
+    RelativeLayout addBtn;
+    DatabaseReference ref;
+    FirebaseRecyclerAdapter<ChatMessage,chat_rec> adapter;
+    Boolean flagFab = true;
+    private TextToSpeech tts;
+    String reply;
+    String res;
+
+    private AIService aiService;
+
+    @Override
+    protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},1);
+
+        tts = new TextToSpeech(this, this);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        editText = (EditText)findViewById(R.id.editText);
+        addBtn = (RelativeLayout)findViewById(R.id.addBtn);
+
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ref = FirebaseDatabase.getInstance().getReference();
+        ref.keepSynced(true);
+
+        final AIConfiguration config = new AIConfiguration("64b30a95302c4d41837e4b5d490c5e2d",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+        final AIDataService aiDataService = new AIDataService(config);
+
+        final AIRequest aiRequest = new AIRequest();
+
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String message = editText.getText().toString().trim();
+
+                if (!message.equals("")) {
+
+                    ChatMessage chatMessage = new ChatMessage(message, "user");
+                    ref.child("chat").push().setValue(chatMessage);
+
+                    aiRequest.setQuery(message);
+                    new AsyncTask<AIRequest,Void,AIResponse>(){
+
+                        @Override
+                        protected AIResponse doInBackground(AIRequest... aiRequests) {
+                            final AIRequest request = aiRequests[0];
+                            try {
+                                final AIResponse response = aiDataService.request(aiRequest);
+                                return response;
+                            } catch (AIServiceException e) {
+                            }
+                            return null;
+                        }
+                        @Override
+                        protected void onPostExecute(AIResponse response) {
+                            if (response != null) {
+
+                                Result result = response.getResult();
+                                 reply = result.getFulfillment().getSpeech();
+                                ChatMessage chatMessage = new ChatMessage(reply, "bot");
+                                ref.child("chat").push().setValue(chatMessage);
+                                openintent();
+
+                            }
+                        }
+                    }.execute(aiRequest);
+                }
+                else {
+                    aiService.startListening();
+                }
+
+                editText.setText("");
+
+            }
+        });
+
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ImageView fab_img = (ImageView)findViewById(R.id.fab_img);
+               Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.ic_send_white_24dp);
+                Bitmap img1 = BitmapFactory.decodeResource(getResources(),R.drawable.ic_mic_white_24dp);
+
+
+                if (s.toString().trim().length()!=0){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img);
+
+
+                }
+                else if (s.toString().trim().length()==0){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img1);
+                    flagFab=true;
+
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        adapter = new FirebaseRecyclerAdapter<ChatMessage, chat_rec>(ChatMessage.class,R.layout.msglist,chat_rec.class,ref.child("chat")) {
+            @Override
+            protected void populateViewHolder(chat_rec viewHolder, ChatMessage model, int position) {
+
+                if (model.getMsgUser().equals("user")) {
+
+
+                    viewHolder.rightText.setText(model.getMsgText());
+
+                    viewHolder.rightText.setVisibility(View.VISIBLE);
+                    viewHolder.leftText.setVisibility(View.GONE);
+                }
+                else {
+                    viewHolder.leftText.setText(model.getMsgText());
+                    res=model.getMsgText();
+
+
+                    viewHolder.rightText.setVisibility(View.GONE);
+                    viewHolder.leftText.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+
+                int msgCount = adapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (msgCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    recyclerView.scrollToPosition(positionStart);
+
+                }
+
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void openintent() {
+        switch(reply){
+
+            case "opening music":
+            {
+                Intent intent = getApplicationContext().getPackageManager().getLaunchIntentForPackage("https://www.crunchbase.com/search/organization.companies");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+            }
+            case "opening amazon for casual shoes" :
+            {
+
+            }
+
+        }
+        if (reply.equals("opening music"))
+        {
+            Intent intent = getApplicationContext().getPackageManager().getLaunchIntentForPackage("https://www.crunchbase.com/search/organization.companies");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+                  /*Intent i = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.crunchbase.com/search/organization.companies"));*/
+
+            startActivity(i);
+        }
+
+    }
+
+    public void ImageViewAnimatedChange(Context c, final ImageView v, final Bitmap new_image) {
+        final Animation anim_out = AnimationUtils.loadAnimation(c, R.anim.zoom_out);
+        final Animation anim_in  = AnimationUtils.loadAnimation(c, R.anim.zoom_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                v.setImageBitmap(new_image);
+                anim_in.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {}
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {}
+                    @Override
+                    public void onAnimationEnd(Animation animation) {}
+                });
+                v.startAnimation(anim_in);
+            }
+        });
+        v.startAnimation(anim_out);
+    }
+
+    @Override
+    public void onResult(ai.api.model.AIResponse response) {
+
+
+        Result result = response.getResult();
+
+        String message = result.getResolvedQuery();
+        ChatMessage chatMessage0 = new ChatMessage(message, "user");
+        ref.child("chat").push().setValue(chatMessage0);
+
+
+        reply = result.getFulfillment().getSpeech();
+        speakOut();
+        openmusic();
+
+        ChatMessage chatMessage = new ChatMessage(reply, "bot");
+        ref.child("chat").push().setValue(chatMessage);
+
+
+    }
+
+    @Override
+    public void onError(ai.api.model.AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+
+    }
+
+    private void speakOut() {
+
+        CharSequence text = reply;
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null,"id1");
+    }
+}
